6e2188efb6ed7d71ca713610fe0948bffbd62716
Sifat-E-Sadakin <sifatesadakin10@gmail.comm>
2023-07-13 16:31:57 +0600

    first draft

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const app = express()
 const port = 3000
 const cors = require('cors')
 require('dotenv').config()
-
+app.use(express.json())
 app.use(cors())
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_U}:${process.env.DB_P}@cluster0.rohhp7w.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -31,6 +31,49 @@ async function run() {
         let result = users;
         res.send(result);
     })
+
+    app.get('/users/:id', async(req, res)=>{
+      let id = req.params.id
+      let find ={ _id : new ObjectId(id)}
+
+      let result =await usersCollection.findOne(find);
+      res.send(result);
+  })
+
+    app.post('/addUser', async (req, res)=>{
+      let data = req.body;
+      let result = await usersCollection.insertOne(data);
+      res.send(result)
+    })
+
+    app.put('/updateUser/:id', async (req, res)=>{
+      let data = req.body;
+      let id = req.params.id
+
+      let filter = { _id: new ObjectId(id) };
+   
+      let options = { upsert: true };
+    
+    let updateDoc = {
+      $set: {
+        name : data.name,
+        email : data.email,
+        phone : data.phone
+      },
+    };
+      let result = await usersCollection.updateOne(filter, updateDoc, options);
+      res.send(result)
+    })
+
+    app.delete('/remove/:id', async(req, res)=>{
+      let id = req.params.id
+
+      let find = { _id: new ObjectId(id) };
+
+      let result = await usersCollection.deleteOne(find);
+   
+      res.send(result);
+  })
 
 
     await client.connect();
